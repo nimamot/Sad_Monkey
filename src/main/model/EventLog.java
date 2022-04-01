abee307eb8c0d5905f70a9c6ba446d976a9f6669
nimamot <64922998+nimamot@users.noreply.github.com>
2022-04-01 11:56:03 -0700

    fix bugs

--- src/main/model/EventLog.java
+++ src/main/model/EventLog.java
@@ -54,6 +54,7 @@ public class EventLog implements Iterable<Event> {
      */
     public void clear() {
         events.clear();
+        logEvent(new Event("Event log cleared."));
     }
 
     @Override
